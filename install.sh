3ffa1b5cb08cd5deb4f8c6e45c4f65760b301d45
Jeremy Lipson <jeremy04@gmail.com>
2025-06-27 13:04:12 -0400

    chore: i thought i committed this

--- install.sh
+++ install.sh
@@ -9,13 +9,6 @@ cat >> "$append" <<'EOF'
 # --- dotfiles-codespace-marker ---
 # Your bashrc goodness goes here
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init - bash)"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 parse_git_branch() {
   git rev-parse --abbrev-ref HEAD 2>/dev/null | sed 's/.*/ (\0)/'
 }
